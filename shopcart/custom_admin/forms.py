fceee81c365fceeced6c2edb7589fb158e88b84f
Mohit1166 <mohinanimohit9@gmail.com>
2022-09-05 12:53:16 +0530

    added gitignore

--- shopcart/custom_admin/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from custom_admin.models import *
-
-class BannersForm(forms.ModelForm):  
-    class Meta:  
-        model = Banners  
-        fields = "__all__"
-        
